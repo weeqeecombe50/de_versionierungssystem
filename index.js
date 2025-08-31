a21f5173fc5903d3235fa8bee76005e2be5ad4b4
weeqeecombe50 <mynieagbona@hotmail.com>
2025-08-31 18:14:27 +0300

    Added a greeting for French version.

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log('Welcome to the Control System - English Version');
 console.log('Bienvenido al Sistema de Control - Versión en Español');
+console.log('Bienvenue dans le Système de Contrôle - Version Française');
